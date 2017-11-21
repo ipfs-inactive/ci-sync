5dbf79b3781cf8f3c7686c4fc7941654137dd8dd
Victor Bjelkholm <victorbjelkholm@gmail.com>
2017-11-21 17:45:54 +0100

    CI team for all repos, now for many orgs

--- ci-team-for-all-repos.js
+++ ci-team-for-all-repos.js
@@ -1,5 +1,12 @@
 (async () => {
-  const org = 'libp2p'
+  const orgs = [
+    'ipfs',
+    'ipfs-shipyard',
+    'libp2p',
+    'multiformats',
+    'ipld',
+    'gxed'
+  ]
   const authKey = process.env.GITHUB_TOKEN
   if (!authKey) {
     throw new Error('Environment variable `GITHUB_TOKEN` has to be defined')
@@ -13,59 +20,61 @@
     token: authKey
   })
 
-  let ciTeamID = null
-  try {
-    const orgTeamsRes = await github.orgs.getTeams({org})
-    const orgTeams = orgTeamsRes.data
-    for (let team of orgTeams) {
-      if (team.slug === 'ci') {
-        ciTeamID = team.id
-      }
-    }
-    if (!ciTeamID) {
-      throw new Error('Could not find the CI team in this org, please create it')
-    }
-  } catch (err) {
-    console.log(err)
-    process.exit(1)
-  }
-
-  let repos = []
-  try {
-    let res = await github.repos.getForOrg({
-      org
-    })
-    repos = repos.concat(res.data)
-    while (github.hasNextPage(res)) {
-      res = await github.getNextPage(res)
-      repos = repos.concat(res.data)
-    }
-  } catch (err) {
-    console.log(err)
-    process.exit(1)
-  }
-  console.log('got all repos', repos.length)
-  for (let repo of repos) {
+  for (let org of orgs) {
+    let ciTeamID = null
     try {
-      const teamsRes = await github.repos.getTeams({
-        owner: repo.owner.login,
-        repo: repo.name
-      })
-      const teams = teamsRes.data.map(t => t.slug)
-      if (teams.includes('ci')) {
-        console.log(repo.full_name + ' already had the ci team')
-      } else {
-        const res = await github.orgs.addTeamRepo({
-          org: repo.owner.login,
-          repo: repo.name,
-          id: ciTeamID,
-          permission: 'push'
-        })
-        console.log(repo.full_name + ' now has ci team')
+      const orgTeamsRes = await github.orgs.getTeams({org})
+      const orgTeams = orgTeamsRes.data
+      for (let team of orgTeams) {
+        if (team.slug === 'ci') {
+          ciTeamID = team.id
+        }
+      }
+      if (!ciTeamID) {
+        throw new Error('Could not find the CI team in this org, please create it')
       }
     } catch (err) {
       console.log(err)
       process.exit(1)
+    }
+
+    let repos = []
+    try {
+      let res = await github.repos.getForOrg({
+        org
+      })
+      repos = repos.concat(res.data)
+      while (github.hasNextPage(res)) {
+        res = await github.getNextPage(res)
+        repos = repos.concat(res.data)
+      }
+    } catch (err) {
+      console.log(err)
+      process.exit(1)
+    }
+    console.log('got all repos', repos.length)
+    for (let repo of repos) {
+      try {
+        const teamsRes = await github.repos.getTeams({
+          owner: repo.owner.login,
+          repo: repo.name
+        })
+        const teams = teamsRes.data.map(t => t.slug)
+        if (teams.includes('ci')) {
+          console.log(repo.full_name + ' already had the ci team')
+        } else {
+          const res = await github.orgs.addTeamRepo({
+            org: repo.owner.login,
+            repo: repo.name,
+            id: ciTeamID,
+            permission: 'push'
+          })
+          console.log(repo.full_name + ' now has ci team')
+        }
+      } catch (err) {
+        console.log(err)
+        process.exit(1)
+      }
     }
   }
 })()
